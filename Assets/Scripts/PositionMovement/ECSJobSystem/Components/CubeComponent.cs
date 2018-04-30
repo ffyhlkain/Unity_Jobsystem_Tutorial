41abfc60454058f89b50178c821d334b95de3eac
phenschel <melchor@gmx.at>
2018-04-30 19:13:48 +0200

    restructured and fixed cubemovement system

---
+++ Assets/Scripts/PositionMovement/ECSJobSystem/Components/CubeComponent.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+using Unity.Transforms2D;
+
+namespace Assets.Scripts.PositionMovement.ECSJobSystem.Components
+{
+    public struct CubeComponent : IComponentData
+    {
+        public Position2D EndPosition;
+    }
+}
