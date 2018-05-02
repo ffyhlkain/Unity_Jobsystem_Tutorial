b311c5144631784236c7b2e45153f1255a2e1bef
Patrick Henschel <patrick.henschel@xyrality.com>
2018-05-02 15:30:27 +0200

    added dummy ECS job system variant

---
+++ Assets/Scripts/PositionMovement/ECSJobSystem/CubeSpawner.cs
@@ -0,0 +1,34 @@
+﻿using Assets.Scripts.PositionMovement.ECSJobSystem.Components;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms2D;
+using UnityEngine;
+
+namespace Assets.Scripts.PositionMovement.ECSJobSystem
+{
+    public class CubeSpawner : MonoBehaviour
+    {
+        [SerializeField] private int cubeCount = 10000;
+
+        private void Start()
+        {
+            InitializeGame();
+        }
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+        public void InitializeGame()
+        {
+            var entityManager = World.Active.GetExistingManager<EntityManager>();
+
+            for (int i = 0; i < cubeCount; i++)
+            {
+                var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                var cubeEntity = cube.AddComponent<GameObjectEntity>().Entity;
+
+                var endPosition = new Position2D();
+                endPosition.Value = new float2(Random.Range(-1000, 1000), Random.Range(-1000, 1000));
+                entityManager.AddComponentData(cubeEntity, new CubeComponent() { EndPosition = endPosition });
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PositionMovement/ECSJobSystem/System/CubeMovementSystem.cs
@@ -0,0 +1,34 @@
+﻿using Assets.Scripts.PositionMovement.ECSJobSystem.Components;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Assets.Scripts.PositionMovement.ECSJobSystem
+{
+    public class CubeMovementSystem : JobComponentSystem
+    {
+        private struct Data
+        {
+            public int Length;
+            public ComponentDataArray<CubeComponent> CubeComponents;
+            public ComponentArray<Transform> Transforms;
+        }
+
+        [Inject] private Data data;
+
+        protected override void OnUpdate()
+        {
+            var passedTime = Time.deltaTime;
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                var entityTransform = data.Transforms[i];
+                var entity = data.CubeComponents[i];
+                var pos = new float2(entityTransform.position.x, entityTransform.position.y);
+                var entityDirection = (entity.EndPosition.Value - pos);
+                var movement = entityDirection * passedTime;
+                entityTransform.position += new Vector3(movement.x, movement.y, 0).normalized;
+            }
+        }
+    }
+}
