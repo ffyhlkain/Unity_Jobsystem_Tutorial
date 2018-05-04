28b3605809a680da68b37cbb2df6fac60e7d1877
phenschel <melchor@gmx.at>
2018-05-04 09:32:27 +0200

    using position instead of position2d

--- Assets/Scripts/PositionMovement/ECSJobSystem/Component/CubeComponent.cs
+++ Assets/Scripts/PositionMovement/ECSJobSystem/Component/CubeComponent.cs
@@ -1,10 +1,10 @@
 ﻿using Unity.Entities;
-using Unity.Transforms2D;
+using Unity.Transforms;
 
 namespace Assets.Scripts.PositionMovement.ECSJobSystem.Components
 {
     public struct ECSJobSystemCubeComponent : IComponentData
     {
-        public Position2D EndPosition;
+        public Position EndPosition;
     }
 }
